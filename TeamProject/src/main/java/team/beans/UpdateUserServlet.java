675bc7bbecf958b5d00a514f0dc22dd0232e98cd
SeoKeSim <rrg0916@naver.com>
2024-11-28 04:35:12 +0900

    ㅁ

--- TeamProject/src/main/java/team/beans/UpdateUserServlet.java
+++ TeamProject/src/main/java/team/beans/UpdateUserServlet.java
@@ -29,7 +29,7 @@ public class UpdateUserServlet extends HttpServlet {
         
         try {
             UserDAO userDAO = new UserDAO();
-            boolean isUpdated = userDAO.updateUser(user);
+            boolean isUpdated = userDAO.updateUserInfo(user);
             
             if(isUpdated) {
                 response.sendRedirect("updateSuccess.jsp");
