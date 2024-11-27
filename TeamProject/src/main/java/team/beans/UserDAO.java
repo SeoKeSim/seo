d403d430dd5dd1aaf6f97f218ace5b99a4882135
20211034 <junsung5130@naver.com>
2024-11-28 01:49:47 +0900

    로그인 기능 구현

--- TeamProject/src/main/java/team/beans/UserDAO.java
+++ TeamProject/src/main/java/team/beans/UserDAO.java
@@ -5,29 +5,27 @@ import java.sql.*;
 
 public class UserDAO {
 
-    public UserDTO UserCheck(String id) throws SQLException{
-        Connection conn = null;
+    public boolean login(String id, String password) {
+      Connection conn = null;
         PreparedStatement stmt = null;
         ResultSet rs = null;
-        UserDTO uDTO = null;
-
+        boolean loginCon = false;
         try {
             conn = JDBCUtil.getConnection();
-            String sql = "SELECT id, name, email FROM users WHERE id = ?";
+            String sql = "SELECT id, password FROM users WHERE id = ? and password = ?";
+            
             stmt = conn.prepareStatement(sql);
             stmt.setString(1, id);
+            stmt.setString(2, password);
             rs = stmt.executeQuery();
-
-            if (rs.next()) {
-            	uDTO = new UserDTO();
-            	uDTO.setId(rs.getString("id"));
-            	uDTO.setName(rs.getString("name"));
-            	uDTO.setEmail(rs.getString("email"));
-            }
+            loginCon = rs.next();
+        } catch (Exception ex) {
+            System.out.println("Exception" + ex);
         } finally {
-            JDBCUtil.close(rs, stmt, conn); // 자원 반납
+        	JDBCUtil.close(rs, stmt, conn);
         }
-
-        return uDTO;
+        return loginCon;
     }
+            
+            
 }
