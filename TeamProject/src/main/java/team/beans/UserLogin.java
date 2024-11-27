4b27e2482e23352cd5ad42ed07d4cab31b3c93f7
SeoKeSim <rrg0916@naver.com>
2024-11-28 04:57:06 +0900

    어드민 로그인 절반 성공

--- TeamProject/src/main/java/team/beans/UserLogin.java
+++ TeamProject/src/main/java/team/beans/UserLogin.java
@@ -12,28 +12,27 @@ public class UserLogin extends HttpServlet {
 
 @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    request.setCharacterEncoding("UTF-8");
-    String id = request.getParameter("id"); // URL 파라미터에서 'id' 값 가져오기
+    	request.setCharacterEncoding("UTF-8");
+    	String id = request.getParameter("id"); // URL 파라미터에서 'id' 값 가져오기
         String password = request.getParameter("password");
              
         UserDAO uDAO = new UserDAO(); // DAO 객체 생성
         boolean loginCheck = uDAO.login(id, password);
             
-            if(loginCheck){
-        request.setAttribute("loginResult", loginCheck);
-    HttpSession session = request.getSession();
-    session.setAttribute("idKey", id);
-    RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
-    dispatcher.forward(request, response);
-
-    }else{
-          response.sendRedirect("LogError.jsp");
-    }
-       
-
-      
-    }
-
-
-    
+        if(loginCheck && id.equals("admin")){
+        	request.setAttribute("loginResult", loginCheck);
+        	HttpSession session = request.getSession();
+        	session.setAttribute("idKey", id);
+        	RequestDispatcher dispatcher = request.getRequestDispatcher("adminPage/admin.jsp");
+        	dispatcher.forward(request, response);
+        }else if(loginCheck){
+        	request.setAttribute("loginResult", loginCheck);
+        	HttpSession session = request.getSession();
+        	session.setAttribute("idKey", id);
+        	RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+        	dispatcher.forward(request, response);
+        }else {
+        	response.sendRedirect("LogError.jsp");
+        }
+    }  
 }
